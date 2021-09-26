2e93731a8b005fa32a85a8c18be24aeacb0dd6e1
Xavier C <xav.canton@gmail.com>
2021-09-26 12:56:57 +0200

    Refactor file downloads to asynchronous

--- src/sport_betting/data_retrieval/betfair/download/dl_files.py
+++ src/sport_betting/data_retrieval/betfair/download/dl_files.py
@@ -1,11 +1,25 @@
 # coding: utf-8
 import asyncio
 import datetime
+import functools
+import os
+from collections import namedtuple
 
 import betfairlightweight
 from pandas import DataFrame
 
 from sport_betting.data_retrieval.config.config import APIConfig
+
+DatedListFiles = namedtuple("DatedListFiles", ["date", "file_name"])
+
+
+def run_in_executor(f):
+    @functools.wraps(f)
+    async def inner(*args, **kwargs):
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(None, lambda: f(*args, **kwargs))
+
+    return inner
 
 
 def get_list_game_days(df_games: DataFrame) -> list:
@@ -20,7 +34,8 @@ def get_list_game_days(df_games: DataFrame) -> list:
     return list_dates
 
 
-async def get_files_to_dl_day(game_day: datetime.datetime, betfair_trading: betfairlightweight.APIClient) -> list:
+@run_in_executor
+def get_files_to_dl_day(game_day: datetime.datetime, betfair_trading: betfairlightweight.APIClient) -> list:
     """
     Retrieve list of files to download for a specific game day
     :param game_day: date
@@ -47,10 +62,11 @@ async def get_files_to_dl_day(game_day: datetime.datetime, betfair_trading: betf
     # Get list files
     list_files = betfair_trading.historic.get_file_list(**data)
 
-    return list_files
+    return [DatedListFiles(game_day.strftime('%Y%m%d'), file) for file in list_files]
 
 
-async def dowload_file(file_path: str, dl_directory: str, betfair_trading: betfairlightweight.APIClient):
+@run_in_executor
+def download_file(file_path: DatedListFiles, dl_directory: str, betfair_trading: betfairlightweight.APIClient):
     """
     Download file from betfair
     :param file_path: Betfair file path
@@ -58,9 +74,15 @@ async def dowload_file(file_path: str, dl_directory: str, betfair_trading: betfa
     :param betfair_trading: instance of the betfair trading API
     :return:
     """
+    # Create directory path
+    directory_path = os.path.join(dl_directory, file_path.date)
+
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path, exist_ok=True)
+
     # Download file
-    betfair_trading.historic.download_file(file_path=file_path,
-                                           store_directory=dl_directory)
+    betfair_trading.historic.download_file(file_path=file_path.file_name,
+                                           store_directory=directory_path)
 
 
 async def download_betfair_files(df_games: DataFrame, dl_directory: str):
@@ -77,18 +99,19 @@ async def download_betfair_files(df_games: DataFrame, dl_directory: str):
     api_cfg = APIConfig()
     trading = betfairlightweight.APIClient(username=api_cfg.betfair_user,
                                            password=api_cfg.betfair_pwd,
-                                           app_key=api_cfg.betfair_certs,
+                                           app_key=api_cfg.betfair_api_key,
                                            certs=api_cfg.betfair_certs)
     trading.login()
 
     # Get list of files to DL
-    list_files = await asyncio.gather(*[get_files_to_dl_day(day, trading) for day in list_game_days],
-                                      return_exceptions=True)
+    list_files_tasks = [asyncio.create_task(get_files_to_dl_day(day, trading)) for day in list_game_days]
+    list_files_nested = [await t for t in asyncio.as_completed(list_files_tasks)]
+
     # Unnest list of files
-    list_files = [item for sublist in list_files for item in sublist]
+    list_files = [item for sublist in list_files_nested for item in sublist]
 
     # Download files
-    dl_files = await asyncio.gather(*[dowload_file(file, dl_directory, trading) for file in list_files],
-                                    return_exceptions=True)
+    list_dl_tasks = [asyncio.create_task(download_file(file, dl_directory, trading)) for file in list_files]
+    results = [await t for t in asyncio.as_completed(list_dl_tasks)]
 
     trading.logout()
