18f96d78a05f87dae15522b6a908912301ead516
Xavier C <xav.canton@gmail.com>
2021-10-06 20:15:14 +0200

    Fix error in bash command

--- src/sport_betting/data_retrieval/clean_files/task.py
+++ src/sport_betting/data_retrieval/clean_files/task.py
@@ -6,6 +6,7 @@ import luigi
 from luigi.mock import MockTarget
 
 from sport_betting import DATA_DIR
+from sport_betting.data_retrieval.betfair.event_matching.task import TaskMatchingEvents
 from sport_betting.data_retrieval.betfair.markets.task import TaskMarkets
 
 
@@ -18,7 +19,8 @@ class TaskCleanFiles(luigi.Task):
         return MockTarget(output_path)
 
     def requires(self):
-        return TaskMarkets(year=self.year, competition_id=self.competition_id)
+        yield TaskMarkets(year=self.year, competition_id=self.competition_id)
+        yield TaskMatchingEvents(year=self.year, competition_id=self.competition_id)
 
     def run(self):
         data_dir = os.path.dirname(self.input().path)
@@ -26,7 +28,7 @@ class TaskCleanFiles(luigi.Task):
         # Delete all temporary files directories
         for directory in [os.path.join(data_dir, el) for el in os.listdir(data_dir)
                           if os.path.isdir(os.path.join(data_dir, el))]:
-            subprocess.call(['rm', 'r', directory])
+            subprocess.call(['rm', '-r', directory])
 
         with self.output().temporary_path() as tmp_path:
             with open(tmp_path, 'w') as writer:
